200dea62a5888a32250ca61a86cfe33db2d8e37c
Jeronimo <jeronimocomaschi05@gmail.com>
2023-03-29 18:18:28 -0300

    uniendo el mobile-menu

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu =  document.querySelector('.mobile-menu')
+const burgericon = document.querySelector('.menu')
+
 
 navEmail.addEventListener('click', () => {
     desktopMenu.classList.toggle('inactive')
+})
+
+burgericon.addEventListener('click', () => {
+    mobileMenu.classList.toggle('inactive')
 })
 
